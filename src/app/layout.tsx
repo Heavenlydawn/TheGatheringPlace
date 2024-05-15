822dd4725464b0355d1b39f08a76d846abcece56
Heaven <gabrielheavenlydawn@gmail.com>
2024-05-15 14:11:32 +0100

    chore: global stylings added

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: "100"
+});
+
+export const metadata: Metadata = {
+  title: "The Gathering Place",
+  description: "Think Celestial",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
