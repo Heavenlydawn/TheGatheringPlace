42e868c5067bff19cc15fb81ca524d59b44a4126
Heaven <gabrielheavenlydawn@gmail.com>
2024-05-16 11:29:18 +0100

    chore: hero section styled props added

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Line from "../../../public/images/Line.png";
-import Button from "../Button/Button"
+import Button from "../Button/Button";
 const Hero = () => {
   return (
-    <div id="hero" className="text-white px-40 py-72">
+    <div id="hero" className="text-white px-40 py-80">
       <h3 className="gp-text-h5">
         WELCOME TO THE <br />
         GATHERING PLACE
@@ -14,17 +14,15 @@ const Hero = () => {
         BECOME A PART OF <br />
         OUR COMMUNITY
       </h1>
-
-      {/* <button className="gp-text-h5 text-black gp-brown py-5 px-8 rounded-md my-12">
-        Contact Us
-      </button> */}
-     <Link href="/mormon"> <Button text="Learn More" /></Link>
+      <Link href="/mormon">
+        <Button text="Learn More" />
+      </Link>
 
       <div className="flex my-12 items-center gap-4">
         <Image src={Line} alt="Line" />
         <p className="gp-text-h6">
-          A community that is valued and relevant to <br />development and growth at
-          all Levels.
+          A community that is valued and relevant to <br />
+          development and growth at all Levels.
         </p>
       </div>
     </div>
--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -1,15 +1,45 @@
-import React from 'react'
-import Header from '../Header/Header'
-import Hero from '../Hero/Hero'
-import HeroCrumb from '../HeroCrumb/HeroCrumb'
+import React from "react";
+import Header from "../Header/Header";
+import Hero from "../Hero/Hero";
+import HeroCrumb from "../HeroCrumb/HeroCrumb";
+import AboutUsIcon from "../../../public/images/aboutUsIcon.svg";
+import GetSkillsIcon from "../../../public/images/getSkillsIcon.svg";
+import LearningIcon from "../../../public/images/learningIcon.svg";
 const Home = () => {
   return (
-    <div>
-        <Header />
-        <Hero />
-        <HeroCrumb />
-    </div>
-  )
-}
+    <div id="home">
+      <Header />
+      <Hero />
 
-export default Home
+      <div className="my-12 text-center">
+        <h3 className="gp-text-h5">GATHERING PLACE</h3>
+        <h1 className="gp-text-h2">A COMMUNITY THAT IS RELEVANT</h1>
+      </div>
+
+  <div className="flex items-center justify-around">
+  <HeroCrumb
+        icon={AboutUsIcon}
+        title="ABOUT US"
+        text="A community of innovative young adults coming together to learn of and
+        feel the Love of the Savior, Jesus Christ."
+      />
+  <HeroCrumb
+        icon={GetSkillsIcon}
+        title="GET SKILLS"
+        text="A community of innovative young adults coming together to learn of and
+        feel the Love of the Savior, Jesus Christ."
+      />
+  <HeroCrumb
+        icon={LearningIcon}
+        title="LEARNING"
+        text="A community of innovative young adults coming together to learn of and
+        feel the Love of the Savior, Jesus Christ."
+      />
+  </div>
+
+
+    </div>
+  );
+};
+
+export default Home;
