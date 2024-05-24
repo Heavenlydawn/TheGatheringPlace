5991a6d6dfd1fadf8e21bb2cf9edb617faedc82b
Heaven <gabrielheavenlydawn@gmail.com>
2024-05-24 12:15:12 +0100

    contact section responsiveness adjusted

--- src/components/ContactUs/ContactHero.tsx
+++ src/components/ContactUs/ContactHero.tsx
@@ -4,10 +4,10 @@ const ContactHero = () => {
   return (
     <section id="contact" className="flex items-center justify-start h-screen">
       <div className="text-white pl-12 md:pl-24">
-        <h2 className="gp-text-h6 leading-7">
+        <h2 className="gp-text-h6 leading-10 mb-8">
           CONTACT
         </h2>
-        <h1 className="gp-text-h1">
+        <h1 className="gp-text-h5 md:gp-text-h3 xl:gp-text-h1">
           GET IN TOUCH WITH <br />
           THE GATHERING PLACE KUBWA STAKE
         </h1>
--- src/components/ContactUs/ContactHeroForm.tsx
+++ src/components/ContactUs/ContactHeroForm.tsx
@@ -3,7 +3,7 @@ import ContactForm from "./ContactForm";
 import ContactDetails from "./ContactDetails";
 const ContactHeroForm = () => {
   return (
-    <div className="grid grid-cols-1 xl:grid-cols-2 items-center justify-center gap-24 px-12 md:px-28 py-24 md:py-32">
+    <div className="grid grid-cols-1 xl:grid-cols-2 items-center justify-center gap-24 px-4 md:px-28 py-24 md:py-32">
       <ContactForm />
       <ContactDetails />
     </div>
