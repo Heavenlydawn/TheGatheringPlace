2368cc8f3f6d906f1e41fa890bcf36736a22b7ff
Heaven <gabrielheavenlydawn@gmail.com>
2024-05-24 12:26:21 +0100

    chore: responsiveness adjustment

--- src/components/ContactUs/ContactHero.tsx
+++ src/components/ContactUs/ContactHero.tsx
@@ -4,11 +4,9 @@ const ContactHero = () => {
   return (
     <section id="contact" className="flex items-center justify-start h-screen">
       <div className="text-white pl-12 md:pl-24">
-        <h2 className="gp-text-h6 leading-10 mb-8">
-          CONTACT
-        </h2>
-        <h1 className="gp-text-h5 md:gp-text-h3 xl:gp-text-h1">
-          GET IN TOUCH WITH <br />
+        <h2 className="gp-text-h6 leading-10 mb-4 xl:mb-8">CONTACT</h2>
+        <h1 className="gp-text-h1">
+          GET IN TOUCH WITH <br /> <br />
           THE GATHERING PLACE KUBWA STAKE
         </h1>
       </div>
